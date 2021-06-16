d0c064af0926f89323870711d76f8753d0d2b3a4
Rey <firnasreyhan@gmail.com>
2021-06-16 18:41:29 +0700

    up

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'poinku',
+	'hostname' => 'kristomoyo.com',
+	'username' => 'poinku',
+	'password' => '1sampaipoinku',
+	'database' => 'poinku_db',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
