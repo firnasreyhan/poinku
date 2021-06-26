1fd7c112ccfaa66dedc95681b914608c05afdcbf
Rey <firnasreyhan@gmail.com>
2021-06-27 00:08:56 +0700

    up ketidakhadiran

--- application/views/eventManager/DashboardEventView.php
+++ application/views/eventManager/DashboardEventView.php
@@ -52,7 +52,7 @@
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">
-                                Jumlah Kehadiran</div>
+                                Jumlah Ketidakhadiran</div>
                             <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $jumlah_tidak_hadir; ?></div>
                         </div>
                         <div class="col-auto">
