c279e44e7e6e4080367bb34b736b77ec2fa8b6ed
Rey <firnasreyhan@gmail.com>
2021-07-06 13:45:04 +0700

    up

--- application/controllers/admin/AturanController.php
+++ application/controllers/admin/AturanController.php
@@ -218,8 +218,8 @@ class AturanController extends CI_Controller
                 $kriteria = $this->KriteriaModel->getDetail(['ID_NILAI' => $param]);
                 $data['nilai'] = $this->NilaiModel->getDetailNilai(['ID_NILAI' => $param]);
 
-                print_r(isset($kriteria));
-                print_r(empty($kriteria));
+                print_r(count($kriteria));
+                // print_r(empty($kriteria));
 
                 // $this->load->view('template/header');
                 // $this->load->view('template/sidebar');
